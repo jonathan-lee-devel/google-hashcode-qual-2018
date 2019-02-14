3e01a9e2aa9570f2a336b4ec2225bca588612103
jonathan <jonathan.lee.devel@gmail.com>
2019-02-14 22:14:56 +0000

    Added InputFileReader and demo code to Main

--- src/com/jdevel/Main.java
+++ src/com/jdevel/Main.java
@@ -1,8 +1,38 @@
 package com.jdevel;
 
+import com.jdevel.input.InputFileReader;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Obtain a list of all files within the directory
+        ArrayList<File> inputFileList = InputFileReader.getFileListFromDirectory("/home/jonathan/workspace/google-hashcode-qual18-files");
+
+        try {
+            // Iterate through each file
+            for (File file : inputFileList) {
+                // For each, configure a simulation
+
+                // Read contents of the file
+                ArrayList<String> fileContent = InputFileReader.readLinesFromFile(file);
+                // Obtain the first line, containing the information about the pre-booked rides, then remove from fileContent list
+                String fileHeader = fileContent.get(0);
+                fileContent.remove(0);
+
+
+                // Obtain the integers from the first line in the input file
+                ArrayList<Integer> headerIntegers = InputFileReader.getSeparatedIntegersListFromString(fileHeader, "\\s");
+
+                for (String s : fileContent) {
+                    System.out.println(s);
+                }
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
---
+++ src/com/jdevel/input/InputFileReader.java
@@ -0,0 +1,98 @@
+package com.jdevel.input;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class InputFileReader {
+
+    public static ArrayList<File> getFileListFromDirectory(String directoryPath) {
+        File directory = new File(directoryPath);
+
+        File[] fileArray = directory.listFiles();
+
+        ArrayList<File> fileArrayList = new ArrayList<>(fileArray.length);
+        for (File file : fileArray) {
+            fileArrayList.add(file);
+        }
+
+        return fileArrayList;
+    }
+
+    /**
+     * Reads a specified amount of lines from a file, storing each line as a string in an array list
+     * @param file file to be read from
+     * @param offset number of lines to skip when reading file
+     * @param max maximum number of lines to read from file
+     * @return array list containing all lines specified to be read
+     * @throws IOException possible IOException while using BufferedReader
+     */
+    public static ArrayList<String> readLinesFromFile( File file, int offset, int max ) throws IOException {
+        // Pre-allocate list to contain lines
+        ArrayList<String> lineList = new ArrayList<>(max - offset);
+
+        // Create reader for the file
+        BufferedReader bReader = new BufferedReader(new FileReader(file));
+
+        // Skip through the first 'offset' lines of the file
+        for (int i = 0; i < offset; i++) {
+            bReader.readLine();
+        }
+
+        // Iterate through each line, adding it to the lineList, stopping on max or null
+        String currentLine = "";
+        for (int i = 0; i < max; i++) {
+            // Read next line in file
+            currentLine = bReader.readLine();
+            // Prevent null
+            if (currentLine == null) {
+                break;
+            }
+            // Otherwise add to list
+            lineList.add(currentLine);
+        }
+
+        // Return the list of file lines
+        return lineList;
+    }
+
+    public static ArrayList<String> readLinesFromFile( File file ) throws IOException {
+        // Pre-allocate list to contain lines
+        ArrayList<String> lineList = new ArrayList<>();
+
+        // Create reader for the file
+        BufferedReader bReader = new BufferedReader(new FileReader(file));
+
+        // Iterate through each line, adding it to the lineList, stopping on null
+        String currentLine = "";
+        while ( (currentLine = bReader.readLine()) != null ) {
+            lineList.add(currentLine);
+        }
+
+        // Return the list of file lines
+        return lineList;
+    }
+
+    /**
+     * Iterates through a String, parsing all integers separated by the specified regex separator
+     * @param intString string containing ints which is to be parsed
+     * @param regexSeparator regex expression used to separate each of the integers in the list
+     * @return array list containing all of the parsed integers
+     */
+    public static ArrayList<Integer> getSeparatedIntegersListFromString(String intString, String regexSeparator) {
+        // Allocate list
+        ArrayList<Integer> integerList = new ArrayList<Integer>();
+
+        // Iterate through each of the separated strings, storing each of the parsed integers in list
+        String[] separatedStrings = intString.split(regexSeparator);
+        for (String s : separatedStrings) {
+            integerList.add( Integer.parseInt(s) );
+        }
+
+        // Return the list of integers
+        return integerList;
+    }
+
+}
